a6abe15367a37fc49f301f9787a31aced231ca9a
alryt <rytova.ae@phystech.edu>
2021-04-05 17:21:13 +0300

    сделано

--- solar_main.py
+++ solar_main.py
@@ -2,11 +2,14 @@
 # license: GPLv3
 
 import tkinter
-
 from tkinter.filedialog import *
 from solar_vis import *
 from solar_model import *
 from solar_input import *
+
+time_speed = 0
+space = 0
+start_button = 0
 
 perform_execution = False
 """Флаг цикличности выполнения расчёта"""
